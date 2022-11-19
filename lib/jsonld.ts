b22c2414ffb14f4d2bbc460f85caae26be22b33c
Fedor Indutny <238531+indutny@users.noreply.github.com>
2022-11-19 10:40:02 -0800

    save

--- lib/jsonld.ts
+++ lib/jsonld.ts
@@ -1,5 +1,21 @@
-import * as jsonld from 'jsonld';
+import * as badJSONLD from 'jsonld';
+import AS_CONTEXT from './contexts/as.js';
 
-export async function compact(data: unknown): Promise<unknown> {
-  return jsonld.compact(data);
+const jsonld = (badJSONLD as any).default;
+
+const AS_URL = 'https://www.w3.org/ns/activitystreams';
+
+async function documentLoader(url: string): Promise<unknown> {
+  if (url === AS_URL) {
+    return { contextUrl: null, document: AS_CONTEXT, documentUrl: null };
+  }
+
+  // No online lookups!
+  return { contextUrl: null, document: { '@context': {} }, documentUrl: null };
+}
+
+export async function compact(doc: unknown): Promise<unknown> {
+  return jsonld.compact(doc, AS_URL, {
+    documentLoader,
+  });
 }
--- lib/routes/activity-pub.ts
+++ lib/routes/activity-pub.ts
@@ -1,9 +1,18 @@
 import type { Application } from 'express';
 
 import { BASE_URL, USER, USER_FULL_NAME } from '../config.js';
+import { compact } from '../jsonld.js';
 
 export default (app: Application): void => {
   // TODO(indutny): verify signature
+  app.param('user', (req, res, next, user) => {
+    if (user !== USER) {
+      res.status(404).send({ error: 'user not found' });
+      return;
+    }
+
+    next();
+  });
 
   app.get('/users/:user', (req, res) => {
     const { accept } = req.headers;
@@ -14,18 +23,8 @@ export default (app: Application): void => {
 
     const { user } = req.params;
 
-    if (user !== USER) {
-      res.status(404).send({ error: 'user not found' });
-      return;
-    }
-
     res.type('application/activity+json').send({
-      '@context': [
-        'https://www.w3.org/ns/activitystreams',
-        {
-          toot: 'http://joinmastodon.org/ns#',
-        }
-      ],
+      '@context': 'https://www.w3.org/ns/activitystreams',
       id: `${BASE_URL}/users/${user}`,
       type: 'Person',
       following: `${BASE_URL}/users/${user}/following`,
@@ -37,5 +36,28 @@ export default (app: Application): void => {
 
       // TODO(indutny): public key
     });
+  });
+
+  app.post('/users/:user/inbox', async (req, res) => {
+    const { accept } = req.headers;
+    if (!accept?.startsWith('application/activity+json')) {
+      res.status(404).send('HTML interface not implemented');
+      return;
+    }
+
+    const { user } = req.params;
+
+    let body: any;
+    try {
+      body = await compact(req.body);
+    } catch (error) {
+      res.status(400).send({ error: 'Bad input' });
+      return;
+    }
+
+    const { type, id, actor, object } = body;
+    console.log(body);
+
+    res.status(500).send({ error: 'oops' });
   });
 };
