3310cd38d7e5ebae666f3e9a6b2a22c0d4423b45
Fedor Indutny <238531+indutny@users.noreply.github.com>
2022-11-20 01:20:03 -0800

    Toot context for json-ld

---
+++ lib/contexts/toot.ts
@@ -0,0 +1,5 @@
+export default {
+  "@context": {
+    "toot": "http://joinmastodon.org/ns"
+  }
+};
--- lib/jsonld.ts
+++ lib/jsonld.ts
@@ -1,11 +1,13 @@
 import * as badJSONLD from 'jsonld';
 import AS_CONTEXT from './contexts/as.js';
 import SEC_CONTEXT from './contexts/sec.js';
+import TOOT_CONTEXT from './contexts/toot.js';
 
 const jsonld = (badJSONLD as any).default;
 
 const AS_URL = 'https://www.w3.org/ns/activitystreams';
 const SEC_URL = 'https://w3id.org/security/v1';
+const TOOT_URL = 'http://joinmastodon.org/ns';
 
 async function documentLoader(url: string): Promise<unknown> {
   if (url === AS_URL) {
@@ -16,12 +18,16 @@ async function documentLoader(url: string): Promise<unknown> {
     return { contextUrl: null, document: SEC_CONTEXT, documentUrl: null };
   }
 
+  if (url === TOOT_URL) {
+    return { contextUrl: null, document: TOOT_CONTEXT, documentUrl: null };
+  }
+
   // No online lookups!
   return { contextUrl: null, document: { '@context': {} }, documentUrl: null };
 }
 
 export async function compact(doc: unknown): Promise<unknown> {
-  return jsonld.compact(doc, [AS_URL, SEC_URL], {
+  return jsonld.compact(doc, [AS_URL, SEC_URL, TOOT_URL], {
     documentLoader,
   });
 }
