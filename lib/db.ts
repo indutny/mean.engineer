36428378d1c975a26c9d671fb86c3fbb64347531
Fedor Indutny <238531+indutny@users.noreply.github.com>
2022-11-21 21:38:26 -0800

    little fixes

--- lib/db.ts
+++ lib/db.ts
@@ -89,9 +89,9 @@ export class Database {
   public async saveAuthToken(token: AuthToken): Promise<void> {
     this.db.prepare(`
       INSERT INTO authTokens
-      (username, hash, salt, iterations, createdAt)
+      (username, $title, hash, salt, iterations, createdAt)
       VALUES
-      ($username, $hash, salt, iterations, $createdAt)
+      ($username, $title, $hash, salt, iterations, $createdAt)
     `).run(token.toColumns());
   }
 
