918e270deed6075dd22c896af48b13fb4e253bf3
Fedor Indutny <238531+indutny@users.noreply.github.com>
2022-11-26 12:20:05 -0800

    Oops, drop js file

--- util/create-user.js
+++
@@ -1,9 +0,0 @@
-import { Database } from '../lib/db.js';
-import { User } from '../lib/models/user.js';
-const db = new Database();
-await db.saveUser(await User.create({
-    username: process.argv[2],
-    password: process.argv[3],
-    profileName: process.argv[4],
-    about: process.argv[5],
-}));
